5a48a6027b46df681149065f85eba36f61afc002
Hannah <hburkha1@binghamton.edu>
2021-05-02 00:37:30 -0400

    Complete

--- assignment2/module/Part E/process_list.c
+++ assignment2/module/Part E/process_list.c
@@ -5,8 +5,10 @@
 #include <linux/uaccess.h>
 #include <linux/sched/signal.h>
 #include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/init.h>
 
-#define BUFFER_LENGTH 1000
+#define BUFFER_LENGTH 350
 
 // Declare fop operations
 static int pl_open(struct inode *inode, struct file *file);
@@ -37,8 +39,8 @@ static int __init pl_init(void) {
 		pr_err("FAILED: Register Process List Module Unsuccessful");
 		return error;
 	}
-	p = next_task(&init_task);
 	pr_info("SUCCESS: Process List Module Registered");
+	p = next_task(&init_task);
 	return 0;
 }
 
@@ -53,38 +55,32 @@ static int pl_open(struct inode *inode, struct file *file) {
 	return 0;
 }
 
-//PID=1 PPID=0 CPU=4 STATE=TASK_RUNNING
-
 static ssize_t pl_read(struct file *file, char __user *out, size_t size, loff_t* off) {
+	int rc;
 	int error_count;
-    //char* state = NULL;
+	int buffer_size = 0;
+    char* p_state = NULL;
 
     char buffer[BUFFER_LENGTH];
-    struct task_struct* p2;
-    //struct task_struct* p_parent;
 
-    int buffer_size = strlen(buffer)+1;
+	p_state = process_state(p->state);
 
-    for_each_process(p) {
-    	if(p == p2){
-    		// Get parrent PID
-	    	//p_parent = p->parent;
-	    	// Get process state
-	    	//state = process_state(p->state);
-    		memset(buffer,0,sizeof(char)*BUFFER_LENGTH);
-	    	sprintf(buffer, "PID=%d", p->pid);
-	    	error_count = copy_to_user(out, buffer, buffer_size);
-	    	/*if (error_count != 0) {
-	    		pr_err("FAILED: Failed to write data to end user");
-	    		return errno;
-	    	}*/
+	memset(buffer,0,sizeof(char)*BUFFER_LENGTH);
+	//PID=1 PPID=0 CPU=4 STATE=TASK_RUNNING
+	sprintf(buffer, "PID=%d PPID=%d CPU=%d STATE=%s", p->pid, p->parent->pid, task_cpu(p), p_state);
+	//printk(KERN_INFO"\nPID=%d", p->pid);
+	buffer_size = strlen(buffer)+1;
 
-	    	p = next_task(p2);
-	    	break;
-    	}
+	error_count = copy_to_user(out, buffer, buffer_size);
+	p = next_task(p);
 
-    }
-    return buffer_size; 
+	if(p == &init_task) {
+		rc = 0;
+	} else {
+		rc = buffer_size;
+	}
+	
+    return rc; 
 }
 
 static int pl_close(struct inode *inodep, struct file *filp) {
@@ -94,74 +90,75 @@ static int pl_close(struct inode *inodep, struct file *filp) {
 }
 
 char* process_state(long state) {
-	char* rv;
+	char* rc = "INVALID";
 	switch(state) {
 		case TASK_RUNNING:
-            rv = "TASK_RUNNING";
+            rc = "TASK_RUNNING";
             break;
         case TASK_INTERRUPTIBLE:
-            rv = "TASK_INTERRUPTIBLE";
+            rc = "TASK_INTERRUPTIBLE";
             break;
         case TASK_UNINTERRUPTIBLE:
-            rv = "TASK_UNINTERRUPTIBLE";
+            rc = "TASK_UNINTERRUPTIBLE";
             break;
         case __TASK_STOPPED:
-            rv = "__TASK_STOPPED";
+            rc = "__TASK_STOPPED";
             break;
         case __TASK_TRACED:
-            rv = "__TASK_TRACED";
+            rc = "__TASK_TRACED";
             break;
 		case EXIT_DEAD:
-	    	rv = "EXIT_DEAD";
+	    	rc = "EXIT_DEAD";
 	    	break;
 		case EXIT_ZOMBIE:
-	    	rv = "EXIT_ZOMBIE";
+	    	rc = "EXIT_ZOMBIE";
 	    	break;
-	    //case EXIT_TRACE:
-	    	//rv = "EXIT_ZOMBIE, EXIT_DEAD";
-	    	//break;
+	    case EXIT_TRACE:
+	    	rc = "EXIT_ZOMBIE, EXIT_DEAD";
+	    	break;
 		case TASK_PARKED:
-	    	rv = "TASK_PARKED";
+	    	rc = "TASK_PARKED";
 	    	break;
 		case TASK_DEAD:
-	    	rv = "TASK_DEAD";
+	    	rc = "TASK_DEAD";
 	    	break;
 		case TASK_WAKEKILL:
-	    	rv = "TASK_WAKEKILL";
+	    	rc = "TASK_WAKEKILL";
 	    	break;
 		case TASK_WAKING:
-	    	rv = "TASK_WAKING";
+	    	rc = "TASK_WAKING";
 	    	break;
 		case TASK_NOLOAD:
-	    	rv = "TASK_NOLOAD";
+	    	rc = "TASK_NOLOAD";
 	    	break;
 		case TASK_NEW:
-	    	rv = "TASK_NEW";
+	    	rc = "TASK_NEW";
 	    	break;
 		case TASK_STATE_MAX:
-	    	rv = "TASK_STATE_MAX";
+	    	rc = "TASK_STATE_MAX";
 	    	break;
-	    /*case TASK_KILLABLE:
-	    	rv = "TASK_WAKEKILL, TASK_UNINTERRUPTIBLE";
+	    case TASK_KILLABLE:
+	    	rc = "TASK_WAKEKILL, TASK_UNINTERRUPTIBLE";
 	    	break;
 	    case TASK_STOPPED:
-			rv = "TASK_WAKEKILL , __TASK_STOPPED";
+			rc = "TASK_WAKEKILL, __TASK_STOPPED";
 			break;
-		case _TASK_TRACED:
-			rv = "TASK_WAKEKILL , __TASK_TRACED";
+		case TASK_TRACED:
+			rc = "TASK_WAKEKILL, __TASK_TRACED";
 			break;
 		case TASK_IDLE:
-			rv = "TASK_UNINTERRUPTIBLE , TASK_NOLOAD"; 
+			rc = "TASK_UNINTERRUPTIBLE, TASK_NOLOAD"; 
 			break;
 		case TASK_NORMAL:
-			rv = "TASK_INTERRUPTIBLE , TASK_UNINTERRUPTIBLE";
+			rc = "TASK_INTERRUPTIBLE, TASK_UNINTERRUPTIBLE";
 			break;
 		case TASK_REPORT:
-			rv = "TASK_RUNNING , TASK_INTERRUPTIBLE , TASK_UNINTERRUPTIBLE , __TASK_STOPPED , __TASK_TRACED , EXIT_ZOMBIE , EXIT_DEAD";
-			break;*/
+			rc = "TASK_RUNNING, TASK_INTERRUPTIBLE, TASK_UNINTERRUPTIBLE, __TASK_STOPPED, __TASK_TRACED, EXIT_ZOMBIE, EXIT_DEAD";
+			break;
 		default:
 	    	return "INVALID";
 	}
+	return rc;
 }
 
 module_init(pl_init)
